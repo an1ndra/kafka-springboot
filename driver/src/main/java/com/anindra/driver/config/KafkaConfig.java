3f94ef4aec4360879ade2d652b6d2fbd4c7760bc
Anindra <anindrakarmakar+git@proton.me>
2025-10-16 16:48:45 +0530

    Kafka in SpringBoot3

---
+++ driver/src/main/java/com/anindra/driver/config/KafkaConfig.java
@@ -0,0 +1,17 @@
+package com.anindra.driver.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import static com.anindra.driver.constant.AppConstant.CAB_LOCATION;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name(CAB_LOCATION).build();
+    }
+}
